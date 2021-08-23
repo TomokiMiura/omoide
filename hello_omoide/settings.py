62ae0b44b04b1ccdd335c7d2cca7dc40ab0288ed
TomokiMiura <3ura10moki@gmail.com>
2021-08-23 23:40:55 +0900

    add .env

--- hello_omoide/settings.py
+++ hello_omoide/settings.py
@@ -9,18 +9,25 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+# 2021/08/23追加
+import environ
+# 2021/08/23追加終わり
 from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# 2021/08/23追加
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
+# 2021/08/23追加終わり
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$f)epk2r04z#r#b30da_(q23p$(y5@vdac=gdyi$z%rs-dm4$9'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
