cb1cb0674b26d582b1ff090d1a879c854cfc7090
TomokiMiura <3ura10moki@gmail.com>
2021-08-29 20:05:45 +0900

    edit setting.py for email status.

--- hello_omoide/settings.py
+++ hello_omoide/settings.py
@@ -188,8 +188,8 @@ CACHES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'omoide.contact.official@gmail.com'
-EMAIL_HOST_PASSWORD = 'ogata0215'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 ###############################
